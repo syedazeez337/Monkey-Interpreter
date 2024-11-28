3457c40b887358406268ec56bc7222024cc536b5
Syed Azeez <syedazeez337@gmail.com>
2024-11-28 23:23:56 +0530

    main file

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/syedazeez337/monkey-interpreter/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Fell free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+
+}
